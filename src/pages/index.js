f0652b382e963a30c678a4d4e9d080048966d465
PieterT2000 <info@codebirds.nl>
2020-03-28 19:26:52 +0100

    add svg wave on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,6 +44,13 @@ class HomePage extends Component {
           <h1 className="subTitle">{acf.hero_subtitle}</h1>
           <p className="text">{acf.hero_text}</p>
         </Hero>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#007be0"
+            fill-opacity="1"
+            d="M0,256L60,240C120,224,240,192,360,176C480,160,600,160,720,176C840,192,960,224,1080,234.7C1200,245,1320,235,1380,229.3L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+          ></path>
+        </svg>
         <section id="about">
           <div className="container">
             <div className="col-1">
